842eb94a449948ff8d5e313fa9f6ab6e6424a699
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2023-08-28 03:14:54 -0700

    union, distinct two column
    🔴RE🔴

---
+++ 1789/Primary Department for Each Employee.sql
@@ -0,0 +1,13 @@
+select distinct employee_id, department_id
+from Employee
+where primary_flag ='Y'
+union
+select distinct employee_id, department_id
+from (
+    select employee_id, department_id, count(department_id) over(partition by employee_id) cnt
+    from Employee
+)
+where cnt = 1
+
+-- select employee_id, department_id, count(department_id) over(partition by employee_id) cnt
+-- from Employee
