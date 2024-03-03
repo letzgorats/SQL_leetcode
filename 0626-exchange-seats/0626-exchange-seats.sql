f3ba202cc08f03bc7ce9e305205b769264b2a3fb
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2024-03-03 17:28:31 +0900

    Time: 909 ms (16.62%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,12 +1,28 @@
+# # Write your MySQL query statement below
+# SELECT CASE WHEN mod(id,2) = 0 THEN id-1
+#             WHEN mod(id,2) = 1 AND id+1 
+#             NOT IN (
+#                 SELECT id
+#                 FROM SEAT
+#             ) THEN id
+#             ELSE id+1
+#             END AS id,student
+# FROM Seat
+# ORDER BY id
+
+
 # Write your MySQL query statement below
-SELECT CASE WHEN mod(id,2) = 0 THEN id-1
-            WHEN mod(id,2) = 1 AND id+1 
-            NOT IN (
-                SELECT id
-                FROM SEAT
-            ) THEN id
-            ELSE id+1
-            END AS id,student
+SELECT CASE WHEN id%2=0 THEN id-1
+            WHEN (id%2!=0 AND id = (
+                SELECT max(id)
+                FROM Seat
+            )) THEN id
+            WHEN id%2!=0 THEN id+1
+            END AS id, student
 FROM Seat
 ORDER BY id
+
+
+
+
 
